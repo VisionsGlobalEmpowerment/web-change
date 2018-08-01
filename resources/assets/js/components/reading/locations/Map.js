924deeb4d4a5203789effa94208d4b9ac9ff57cf
Ivan Khaldeev <ikhaldeev@yandex.ru>
2018-08-02 00:55:11 +0400

    Cleanup map location

--- resources/assets/js/components/reading/locations/Map.js
+++ resources/assets/js/components/reading/locations/Map.js
@@ -2,6 +2,48 @@ import React, { Component } from 'react';
 import { Spring, animated, interpolate, config } from 'react-spring'
 import { TimingAnimation, Easing } from 'react-spring/dist/addons.cjs'
 
+function getDuration(to, from) {
+    const duration = Math.sqrt(Math.pow((to.x - from.x), 2) + Math.pow((to.y - from.y), 2)) * 10;
+    if (duration < 100) {
+        return 100;
+    }
+
+    return duration;
+}
+
+function LocationEntrance(props) {
+    return <g transform={'translate(' + props.x + ',' + props.y + ')'} onClick={() => props.onClick()}>
+        <rect x={0} y={0} width={50} height={50} fill={"#161a3c"}
+              className={'location-entrance-' + props.locationKey} />
+        <text x={0} y={25} >{props.name}</text>
+    </g>;
+}
+
+function Character(props) {
+    const {
+        isMoving,
+        from = {},
+        to = {}
+    } = props.movement;
+
+    if (!isMoving) {
+        return <circle cx={250} cy={250} r={10} fill={"#383ce5e"} />;
+    }
+
+    const duration = getDuration(to, from);
+
+    return <Spring native
+                 impl={TimingAnimation}
+                 config={{ duration: duration, easing: Easing.linear }}
+                 from={{ radius: 10, x: from.x, y: from.y }}
+                 to={{ radius: 20, x: to.x, y: to.y }}
+                 onRest={() => props.onMovementFinish(to.key)}>
+            {({ radius, x, y }) => (
+                <animated.circle cx={x} cy={y} r={radius} fill={"#383ce5e"} />
+            )}
+        </Spring>;
+}
+
 export default class Map extends Component {
     state = {
         movement: {isMoving: false}
@@ -71,40 +113,27 @@ export default class Map extends Component {
             height
         } = {width: 600, height: 600};
 
-        const {
-            isMoving,
-            from = {},
-            to = {}
-        } = this.state.movement
-
-        const character = isMoving ?
-            (<Spring native
-                     impl={TimingAnimation}
-                     config={{ duration: 3000, easing: Easing.linear }}
-                     from={{ radius: 10, x: from.x, y: from.y }}
-                     to={{ radius: 20, x: to.x, y: to.y }}
-                     onRest={() => this.onMovementFinish(to.key)}>
-                {({ radius, x, y }) => (
-                    <animated.circle cx={x} cy={y} r={radius} fill={"#383ce5e"} />
-                )}
-            </Spring>) : (<circle cx={250} cy={250} r={10} fill={"#383ce5e"} />)
         return (
             <div className="card">
                 <div className="card-header">Map location</div>
 
                 <div className="card-body">
-                    <svg width={width} height={height}>
+                    <svg viewBox={'0 0 ' + width + ' ' + height}>
                         <rect width={width} height={height} rx={14} fill={"#272b4d"} />
 
-                        {character}
+                        {this.locations
+                            .filter(location => location.hasOwnProperty('x'))
+                            .map(location =>
+                                <LocationEntrance
+                                    key={location.key}
+                                    x={location.x} y={location.y}
+                                    locationKey={location.key}
+                                    name={location.name}
+                                    onClick={() => this.moveTo(location.key)} />
+                            )
+                        }
 
-                        <rect x={250} y={250} width={50} height={50} fill={"#161a3c"}
-                              onClick={() => this.moveTo('home')}
-                              className={'location-entrance-home'} />
-
-                        <rect x={50} y={450} width={50} height={50} fill={"#161a3c"}
-                              onClick={() => this.moveTo('fair')}
-                              className={'location-entrance-fair'} />
+                        <Character movement={this.state.movement} onMovementFinish={this.onMovementFinish}/>
                     </svg>
                 </div>
 
