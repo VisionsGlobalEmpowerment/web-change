9d2dd5db54d68fb9fe2ce44a6f1dce91a58a3bd6
Ivan Khaldeev <ikhaldeev@yandex.ru>
2018-09-14 23:11:56 +0400

    Prepare text bubble

--- resources/assets/js/components/common/Activity.js
+++ resources/assets/js/components/common/Activity.js
@@ -4,13 +4,45 @@ import React from "react";
 export default class Activity extends Component {
     state = {
         activated: false,
-    }
+        currentTextIndex: 0,
+    };
 
     activate() {
         if (!this.state.activated) {
             this.props.onFinish();
-            this.setState({activated: true})
+            this.setState({activated: true});
+
+            this.scheduleNextText();
         }
+    }
+
+    currentTextLengthInMs() {
+        const currentText = this.props.text[this.state.currentTextIndex];
+        return 500 + currentText.length * 100;
+    }
+
+    scheduleNextText() {
+        setTimeout(() => {
+            this.showNextText();
+        }, this.currentTextLengthInMs());
+    }
+
+    showNextText() {
+        const {text} = this.props;
+
+        const nextIndex = this.state.currentTextIndex + 1;
+
+        if (nextIndex >= text.length) {
+            this.deactivate();
+            return;
+        }
+
+        this.setState({currentTextIndex: nextIndex});
+        this.scheduleNextText();
+    }
+
+    deactivate() {
+        this.setState({activated: false, currentTextIndex: 0})
     }
 
     textX(text) {
@@ -22,7 +54,8 @@ export default class Activity extends Component {
             x,
             y,
             activityKey,
-            name
+            name,
+            text
         } = this.props;
 
         const label = name + (this.state.activated ? ' (activated)' : '');
@@ -31,6 +64,13 @@ export default class Activity extends Component {
             <rect x={0} y={0} width={40} height={60} rx={4} fill={"#bdbdbd"}
                   className={'activity-' + activityKey} />
             <text x={this.textX(label)} y={-15} >{label}</text>
+            {this.state.activated &&
+            (<g transform={'translate(-100, -200)'}>
+                <rect width={250} height={150} fill={"#5d8d9d"}/>
+                <foreignObject width={250} height={150} requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility">
+                    <p style={{textAlign: "center", verticalAlign: "middle"}} xmlns="http://www.w3.org/1999/xhtml">{text[this.state.currentTextIndex]}</p>
+                </foreignObject>
+            </g>) }
         </g>;
     }
 }
--- resources/assets/js/components/reading/locations/Home.js
+++ resources/assets/js/components/reading/locations/Home.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import {Character, LocationEntrance, ModalText} from "../../common";
+import {Character, LocationEntrance} from "../../common";
 import Activity from "../../common/Activity";
 
 export default class Home extends Component {
@@ -20,7 +20,17 @@ export default class Home extends Component {
     activities = [{
         key: 'lesson-one-instructions',
         name: 'Lesson one instructions',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis pellentesque metus, vel vehicula lectus. Etiam sit amet sagittis lorem, quis convallis enim. Mauris ut mi a odio euismod pharetra quis sed est. Maecenas ut nulla nibh. Suspendisse risus libero, fringilla ac aliquam non, vestibulum et neque. Etiam tristique condimentum ligula, eu consectetur enim sodales non. Pellentesque a mi ac enim gravida blandit. Vestibulum laoreet a nisi sit amet venenatis. Sed tincidunt, diam ut malesuada ullamcorper, justo urna fermentum dolor, consequat fringilla mi orci sed nibh. Praesent nisl orci, condimentum sit amet nisi et, convallis vulputate nibh. Donec porttitor lectus libero, at lobortis lorem imperdiet non. Fusce fringilla, mi nec pellentesque venenatis, libero elit vehicula orci, a pellentesque justo turpis at eros. Sed at ex sit amet magna molestie ultricies. Etiam aliquam ante in sapien pulvinar, rutrum varius mi feugiat. Fusce condimentum magna eu leo rhoncus, a consectetur nulla ullamcorper. Praesent auctor eros lectus, non volutpat ipsum elementum at.',
+        text: [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis pellentesque metus, vel vehicula lectus.',
+            'Etiam sit amet sagittis lorem, quis convallis enim. Mauris ut mi a odio euismod pharetra quis sed est. Maecenas ut nulla nibh.',
+            'Suspendisse risus libero, fringilla ac aliquam non, vestibulum et neque. Etiam tristique condimentum ligula, eu consectetur enim sodales non.',
+            'Pellentesque a mi ac enim gravida blandit. Vestibulum laoreet a nisi sit amet venenatis.',
+            'Sed tincidunt, diam ut malesuada ullamcorper, justo urna fermentum dolor, consequat fringilla mi orci sed nibh.',
+            'Praesent nisl orci, condimentum sit amet nisi et, convallis vulputate nibh. Donec porttitor lectus libero, at lobortis lorem imperdiet non.',
+            'Fusce fringilla, mi nec pellentesque venenatis, libero elit vehicula orci, a pellentesque justo turpis at eros.',
+            'Sed at ex sit amet magna molestie ultricies. Etiam aliquam ante in sapien pulvinar, rutrum varius mi feugiat.',
+            'Fusce condimentum magna eu leo rhoncus, a consectetur nulla ullamcorper. Praesent auctor eros lectus, non volutpat ipsum elementum at.'
+        ],
         x: 350,
         y: 350
     }];
@@ -39,17 +49,10 @@ export default class Home extends Component {
                 const progress = res.data;
                 this.props.handleProgress(progress);
             })
-            .then(() => {
-                this.setState({currentActivity: activity});
-            })
     }
 
     getLocation = (key) => {
         return this.locations.find(location => location.key === key);
-    }
-
-    getActivity = (key) => {
-        return this.activities.find(activity => activity.key === key);
     }
 
     moveTo = (locationKey) => {
@@ -78,12 +81,6 @@ export default class Home extends Component {
         const map = this.locations[0];
 
         return (
-            <div>
-                {this.state.currentActivity && <ModalText
-                    text={this.getActivity(this.state.currentActivity).text}
-                    name={this.getActivity(this.state.currentActivity).name}
-                    onHide={() => this.setState({currentActivity: null})} />}
-
             <div className="card">
                 <div className="card-header">Home location</div>
 
@@ -109,6 +106,7 @@ export default class Home extends Component {
                                 x={activity.x} y={activity.y}
                                 activityKey={activity.key}
                                 name={activity.name}
+                                text={activity.text}
                                 onFinish={() => this.finishActivity(activity.key)}/>
                         )}
 
@@ -118,7 +116,6 @@ export default class Home extends Component {
                             onMovementFinish={this.onMovementFinish} />
                     </svg>
                 </div>
-            </div>
             </div>
         )
     }
